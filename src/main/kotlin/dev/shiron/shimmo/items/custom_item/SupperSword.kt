af2a080ecff80fd76763f1b2f3ee5ff2c1e95559
shiro4710 <54439127+shiro4710@users.noreply.github.com>
2023-03-14 15:05:58 +0900

    feat: add attack
    回転斬りを追加

--- src/main/kotlin/dev/shiron/shimmo/items/custom_item/SupperSword.kt
+++ src/main/kotlin/dev/shiron/shimmo/items/custom_item/SupperSword.kt
@@ -1,14 +1,20 @@
 package dev.shiron.shimmo.items.custom_item
 
+import dev.shiron.shimmo.Shimmo
 import dev.shiron.shimmo.items.ItemClass
 import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
+import org.bukkit.event.block.Action
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
+import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitRunnable
+
 
 class SupperSword : ItemClass() {
     override val item = ItemStack(Material.DIAMOND_SWORD)
@@ -21,6 +27,26 @@ class SupperSword : ItemClass() {
         meta?.addEnchant(Enchantment.LUCK, 1, false)
         meta?.addItemFlags(ItemFlag.HIDE_ENCHANTS)
         itemMeta = meta
+    }
+
+    override fun onClick(event: PlayerInteractEvent) {
+        if (event.action == Action.RIGHT_CLICK_BLOCK || event.action == Action.RIGHT_CLICK_AIR) {
+            val player = event.player
+            val range = 2.0
+            player.sendMessage("回転斬り")
+
+            PlayerRotation(player) {
+                val entities = player.world.getNearbyEntities(player.location, range, range, range)
+                    .filterIsInstance<LivingEntity>().filter { it !is Player }
+                for (entity in entities) {
+                    entity.damage(10.0)
+                }
+            }.runTaskTimer(
+                JavaPlugin.getPlugin(Shimmo::class.java),
+                0L,
+                1L
+            )
+        }
     }
 
     override fun onEntityDamageByEntity(event: EntityDamageByEntityEvent) {
@@ -43,5 +69,22 @@ class SupperSword : ItemClass() {
             event.isCancelled = true
         }
     }
+}
 
+class PlayerRotation(private val player: Player, private val onFinish: () -> Unit) : BukkitRunnable() {
+    private val originLocation = player.location
+    private var yaw = 0f
+    private val speed = 30
+    override fun run() {
+        yaw += speed
+        if (yaw > 360) {
+            player.teleport(originLocation)
+            onFinish()
+            cancel()
+        }
+
+        val location = originLocation
+        location.yaw = yaw
+        player.teleport(originLocation)
+    }
 }
